c2378ab42b46d71ed64c2aebef00dec5bf72de75
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-15 18:46:09 +0500

    Project Created

---
+++ Ch14-P7-AppConfigReaderApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch14_P7_AppConfigReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
